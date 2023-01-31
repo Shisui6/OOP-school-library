1e8d4b31ff62db2683100cb42dc79abfb5d9e8f6
Shisui <37116322+Shisui6@users.noreply.github.com>
2023-01-31 21:20:21 +0100

    Create teacher class

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,11 @@
+require 'securerandom'
+
 class Person
   attr_accessor(:name, :age)
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = SecureRandom.uuid
     @name = name
     @age = age
     @parent_permission = parent_permission
